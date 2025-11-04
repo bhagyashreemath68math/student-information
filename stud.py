0bb6dd14499813c71074e1e11a69c559097fd68e
bhagyashreemath68math <bhagyashreemath68@gmail.com>
2025-11-04 11:58:23 +0530

    Update stud.py

--- stud.py
+++ stud.py
@@ -1,15 +1,16 @@
 import sys
 
 if len(sys.argv) == 3:
-  script name = sys.argv[0]
-  name = sys.argv[1]
-  rollno = sys.argv[2]
-  print("User provided input values:")
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("User provided input values:")
 else:
-  script name = sys.argv[0]
-  name = "Bhagya"
-  rollno = "323"
-  print("No input given - using default values:")
-print("Script Name:", Script name)
+    script_name = sys.argv[0]
+    name = "Bhagya"
+    rollno = "323"
+    print("No input given - using default values:")
+
+print("Script Name:", script_name)
 print("Student Name:", name)
 print("Roll Number:", rollno)
